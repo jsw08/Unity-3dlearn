16ac69853c7d5945a62f0d9a5b864bdac0f1d868
Jurn Wubben <jurn.wubben@ing.com>
2024-11-10 02:01:32 +0100

    Fixed movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,29 +3,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-  [SerializeField] CharacterController cc;
-  [SerializeField] float speed = 1;
-  private bool movingIsDown = false;
-  private Vector2 movementDirection = Vector2.zero;
+  [SerializeField]
+  private Rigidbody rb;
+
+  [SerializeField]
+  private float speed = 2f;
+
+  [SerializeField]
+  private float jumpHeight = 5f;
+  private Vector2 moveInput;
+  private bool isGrounded = true;
 
   void FixedUpdate()
   {
-    /*Vector3 forward = transform.TransformDirection(transform.forward);*/
-    /*float curSpeed = speed*/
-    /*Debug.Log(finalMovement);*/
-    Vector3 move = new Vector3(movementDirection.x, movementDirection.y);
-    cc.SimpleMove(move * speed);
-  }
-
-  private void OnJump()
-  {
-    /*if (isGrounded)*/
-    /*  rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);*/
-
+    Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y; // Transform.right is zegmaar rotation van gameobject naar vector geconvert zodat je het keer iets kan doen en hetzelfde met forward.
+    rb.AddForce(move.normalized * speed, ForceMode.VelocityChange);
   }
 
   private void OnMove(InputValue value)
   {
-    movementDirection = value.Get<Vector2>();
+    moveInput = value.Get<Vector2>();
+  }
+
+  private void OnJump()
+  {
+    if (!isGrounded) return;
+
+    rb.AddForce(transform.up * jumpHeight, ForceMode.VelocityChange);
+    isGrounded = false;
+  }
+  private void OnCollisionEnter(Collision collission)
+  {
+    if (collission.gameObject.CompareTag("Ground")) isGrounded = true;
   }
 }
